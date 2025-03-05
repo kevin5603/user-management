6a7cd82234d55071aeae2141265de890a55e7347
AnJung <wasabi.neko.owo@gmail.com>
2025-03-05 15:50:32 +0800

    Add User model
    - add user model with 'devise' password
    - add index.html
    - add necessary gem/yarn dependency
    - no role yet

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :phone_number, format: {
+    with: /\A\+?[\d\s\-\(\)]{10,15}\z/,
+    message: "must be a valid phone number"
+  }
+end
