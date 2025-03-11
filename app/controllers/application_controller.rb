c6e9a76a592c1667dfd586d3facad830c5721008
Gina Lin <41601753+ginaz5@users.noreply.github.com>
2025-03-11 10:18:15 +0800

    add mail confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   include DeviseCustomParams
+  before_action :check_email_confirmed, if: :user_signed_in?
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: "You are not authorized to perform this action."
+  end
+
+  private
+
+  def check_email_confirmed
+    if current_user && !current_user.confirmed?
+      sign_out current_user
+      redirect_to new_user_session_path, alert: "You must confirm your email before logging in."
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_06_205509) do
+ActiveRecord::Schema.define(version: 2025_03_10_081853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 2025_03_06_205509) do
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
