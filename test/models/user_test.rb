91aebb4a70862f23c63a232173eb0fd50e96f09f
AnJung <wasabi.neko.owo@gmail.com>
2025-03-13 08:34:36 +0800

    Add test for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,28 +1,47 @@
-require "test_helper"
+require 'test_helper'
+require 'sidekiq/testing'
 
 class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @valid_user_param = {
+      email: 'foo.bar@example.com',
+      password: 'password',
+      first_name: 'Foo',
+      last_name: 'Bar',
+      phone_number: '0912345678',
+    }
+  end
+
   test 'should be able create valid user' do
-    # TODO: test
+    assert User.create!(@valid_user_param)
   end
 
   test 'should not allow invalid phone numbers' do
-    # TODO: test
+    user = User.new(@valid_user_param)
+
+    user.phone_number = '123456'
+    assert_not user.valid?
   end
 
   test 'should able to have multiple roles' do
-    # TODO: test
-  end
-
-  test 'email should be required' do
-    # TODO: test
+    user = User.create!(@valid_user_param)
+    user.roles << Role.new(name: 'role_foo')
+    user.roles << Role.new(name: 'role_bar')
+    user.reload
+    assert_equal user.roles.count, 2
   end
 
   test 'email should be unique' do
-    # TODO: test
+    User.create!(@valid_user_param)
+    user = User.new(@valid_user_param)
+    assert_not user.valid?
   end
 
   test 'should enqueue email job when create new user' do
-    # TODO: test
+    assert_equal 0, RegistrationNotificationEmailJob.jobs.size
+    User.create!(@valid_user_param)
+    assert_equal 1, RegistrationNotificationEmailJob.jobs.size
   end
 
 end
