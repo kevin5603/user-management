045cd1bdb4f7eda1452e6912f5129818de86f1bf
AnJung <wasabi.neko.owo@gmail.com>
2025-03-13 17:01:47 +0800

    Add test for role_test

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -3,14 +3,18 @@ require "test_helper"
 class RoleTest < ActiveSupport::TestCase
 
   test 'name should be required' do
-    # TODO: test
+    assert_not Role.new.valid?
   end
 
   test 'name should be unique' do
-    # TODO: test
+    assert Role.create!(name: 'test')
+    assert_not Role.new(name: 'test').valid?
   end
 
   test 'should be able have multiple permissions' do
-    # TODO: test
+    index_user_permission = Permission.find(ActiveRecord::FixtureSet.identify(:index_users))
+    show_user_permission = Permission.find(ActiveRecord::FixtureSet.identify(:show_users))
+
+    assert Role.create!(name: 'test', permissions: [index_user_permission, show_user_permission])
   end
 end
