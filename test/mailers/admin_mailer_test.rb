3f17251efd52ae9165c86da9762e27047b612895
AnJung <wasabi.neko.owo@gmail.com>
2025-03-13 10:17:04 +0800

    Add test for AdminMailer and job

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -2,6 +2,19 @@ require "test_helper"
 
 class AdminMailerTest < ActionMailer::TestCase
   test 'should retrieve correct admin email list' do
-    # TODO: test
+    sender = 'notifications@example.com'
+    admin1 = FactoryBot.create(:user, :admin)
+    admin2 = FactoryBot.create(:user, :admin)
+    new_user = FactoryBot.create(:user)
+    mail_list = [admin1.email, admin2.email]
+
+    email = AdminMailer.with(user_id: new_user.id).new_registration_notification_email
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal sender, email.from.first
+    assert_equal mail_list, email.to
   end
 end
--- test/sidekiq/registration_notification_email_job_test.rb
+++ test/sidekiq/registration_notification_email_job_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
-class RegistrationNotificationEmailJobTest < Minitest::Test
+require 'sidekiq/testing'
+require 'minitest/mock'
+
+class RegistrationNotificationEmailJobTest < ActiveJob::TestCase
+  def setup
+    FactoryBot.create(:user, :admin)
+    @user = User.create!(email: 'email@example.com', password: 'password')
+  end
+
   test 'sends email when perform' do
-    # TODO: test
+    mock_mailer = Minitest::Mock.new
+    mock_mailer.expect :deliver_now, true
+
+    AdminMailer.stub :new_registration_notification_email, mock_mailer do
+      RegistrationNotificationEmailJob.new.perform(@user.id)
+    end
   end
 end
